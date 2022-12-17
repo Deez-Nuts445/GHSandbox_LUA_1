91d5c864fdf64d95ddffcd9b2f22c56f523c889e
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-12-17 22:45:16 +0800

    Create getclosestplayer.lua

---
+++ getclosestplayer.lua
@@ -0,0 +1,27 @@
+local module = {}
+
+function module.GetClosestPlayer()
+local player = game.Players.LocalPlayer
+local players = game.Players:GetPlayers()
+  local minDistance = math.huge
+  local closestPlayer = nil
+  for _, otherPlayer in pairs(players) do
+    if otherPlayer == player then
+      continue
+    end
+    local distance = (player.Character.PrimaryPart.Position - otherPlayer.Character.PrimaryPart.Position).magnitude
+    if distance < minDistance then
+      closestPlayer = otherPlayer
+      print(closestPlayer)
+      minDistance = distance
+      display = game.Players:FindFirstChild(closestPlayer.Name).DisplayName
+    end
+  end
+if display == closestPlayer.Name then
+return closestPlayer,"has a distance of "..minDistance
+else
+return display..",aka",closestPlayer,"has a distance of "..minDistance
+end
+end
+
+return module
