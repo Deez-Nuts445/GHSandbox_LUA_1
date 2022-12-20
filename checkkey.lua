def09ab64443b67347c5af5865a1cacca2895200
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-12-20 15:02:47 +0800

    Create checkkey.lua

---
+++ checkkey.lua
@@ -0,0 +1,15 @@
+local module = {}
+
+function checkIfKeyPressed(cur,target,callback)
+while wait() do
+  local uis = game:GetService("UserInputService")
+  uis.InputBegan:Connect(function(key,busy)
+   if not busy then
+      if key.KeyCode == target then
+        callback()
+   end       
+ end)    
+end
+end
+
+return module
