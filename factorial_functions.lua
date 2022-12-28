db731c85e7f9e43c8b029d2dd74a6c7080663ef8
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-12-28 22:09:03 +0800

    Create factorial_functions.lua

---
+++ factorial_functions.lua
@@ -0,0 +1,25 @@
+local module = {}
+
+function module.fact(nn)
+local n = tonumber(nn)
+if n <= 0 then
+return 1
+else
+return n * fact(n-1)
+end
+end
+
+function module.unfact(nn)
+local result
+local idx = 1
+local n = tonumber(nn)
+if n <= 1 then
+return 0
+else
+idx = idx+1
+result = n / idx
+return math.floor(result+0.5)
+end
+end
+
+return module
