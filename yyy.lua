c51814ae47937c68b964155cbf29bd5c91bffe9b
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2023-01-27 12:18:52 +0800

    Update yyy.lua

--- yyy.lua
+++ yyy.lua
@@ -7,22 +7,26 @@ local secs = os.date("*t")["sec"]
 local pl1 = hour 
 local pl2 = mint
 local pl3 = secs
-local pl4 = "AM"
+local pl4 = ""
 -- change to 0, with pm/am switching
 if type == 12 then -- checks if type = 12 hour
 if hour >= 10 then -- os.date("*t") returns 24 hour, so this checks for 2 digits
 if hour ~= 12 then -- because os.date("*t") returns 24 hour, so this if else checks if its am or pm
 pl1 = hour - 12
-if pl1 >= 9 then -- checks if pl1(edited time) is smaller than 10
-pl1 = "0"..pl1                                        
-end       
-pl4 = "PM"
+if pl1 >= 10 then
+pl1 = pl1
+        else
+pl1 = "0"..pl1        
+end
+pl4 = 'PM'
 else
-pl4 = "AM"
+pl1 = "0"..hour
+pl4 = 'AM'
+end
 end
 if mint <= 9 then
 pl2 = "0"..mint
-end 
+end
 if secs <= 9 then
 pl3 = "0"..secs
 else
