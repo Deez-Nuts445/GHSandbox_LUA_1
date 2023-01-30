de665ec59ae5225693b610c9bcfe6f87b27816c0
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2023-01-30 21:53:35 +0800

    Create tptool.lua

---
+++ tptool.lua
@@ -0,0 +1,16 @@
+local Players = game.Players
+IYMouse = Players.LocalPlayer:GetMouse()	
+speaker = game.Players.LocalPlayer
+
+	local TpTool = Instance.new("Tool")
+	TpTool.Name = "Teleport Tool"
+	TpTool.RequiresHandle = false
+	TpTool.Parent = speaker.Backpack
+	TpTool.Activated:Connect(function()
+		local Char = speaker.Character or workspace:FindFirstChild(speaker.Name)
+		local HRP = Char and Char:FindFirstChild("HumanoidRootPart")
+		if not Char or not HRP then
+			return warn("Failed to find HumanoidRootPart")
+		end
+		HRP.CFrame = CFrame.new(IYMouse.Hit.X, IYMouse.Hit.Y + 3, IYMouse.Hit.Z, select(4, HRP.CFrame:components()))
+	end)
